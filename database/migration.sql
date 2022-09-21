05cc0cbb14a74bf8c0cd8bd9b37670d051f51c5d
Prakash Rai <prakash.bahing21@gmail.com>
2022-09-21 18:27:21 +0545

    fix:yml db port binding fixed and migration issue fixed

--- database/migration.sql
+++ database/migration.sql
@@ -1,8 +1,10 @@
 CREATE TABLE `users`
 (
-    id bigint auto_increament,
+    id   bigint auto_increment,
     name varchar(255) NOT NULL,
     PRIMARY KEY (`id`)
 );
 
-INSERT INTO `users` VALUES ('John Doe'), ('Jane Doe');
+INSERT INTO `users` (`name`)
+VALUES ('Solomon'),
+       ('Menelik');
